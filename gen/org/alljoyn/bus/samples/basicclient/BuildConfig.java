cf96d78c5a52d61cb68e5bc633fcd4d5f55a18a7
John <avble@gmail.com>
2015-10-27 17:21:52 +0700

    copy from basic (client)

---
+++ gen/org/alljoyn/bus/samples/basicclient/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.alljoyn.bus.samples.basicclient;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
